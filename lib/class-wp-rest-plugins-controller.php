ac8c8d48a2c353fa9e36ffd8b48acdc20dfb64f1
Paul Gibbs <djpaul@gmail.com>
2016-03-28 19:44:43 +0100

    URL encode the plugin ID.
    We need the path and filename to be able to activate/deactivate these
    later.

--- lib/class-wp-rest-plugins-controller.php
+++ lib/class-wp-rest-plugins-controller.php
@@ -174,7 +174,7 @@ class WP_REST_Plugins_Controller extends WP_REST_Controller {
 
 		foreach ( $all_plugins as $type => $_plugins ) {
 			foreach ( $_plugins as $id => $plugin ) {
-				$plugin['id'] = plugin_basename( $id );
+				$plugin['id'] = urlencode( $id );
 
 				if ( $type === 'dropins' || $type === 'mustuse' ) {
 					$plugin['status'] = $type;
