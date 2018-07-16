61a60940ba84caef9f7427587b5c5addd87c6d33
kingingkingdoms <37118190+kingingkingdoms@users.noreply.github.com>
2018-07-15 23:15:18 -0400

    Update loader.php

--- loader.php
+++ loader.php
@@ -1,13 +1,14 @@
 <?php
-/*
-Plugin Name: Millionaire's Digest Matchmaker  
-Description: Give users the ability to find friends and accounts based on their same passions and interests.
-Version: 1.0.0
-Author: K&L (Founder of the Millionaire's Digest)
-Author URI: https://millionairedigest.com/
-*/
+/**
+ * Plugin Name: Millionaire's Digest Matchmaker
+ * Description: Give users the ability to find friends and accounts based on their same passions and interests.
+ * Author: K&L (Founder of the Millionaire's Digest)
+ * Author URI: https://millionairedigest.com/
+ * Version: 1.0.0
+ */
 
-require_once( 'class-mp-bp-match.php' );
-if( class_exists( 'Mp_BP_Match' ) ) {
-  new Mp_BP_Match();
-}
+  require_once('class-mp-bp-match.php');
+
+  if(class_exists('Mp_BP_Match')){
+       new Mp_BP_Match();
+  }
