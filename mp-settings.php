1f1cf72cfec01d3c4b7445ae981f004e478f66da
kingingkingdoms <37118190+kingingkingdoms@users.noreply.github.com>
2018-07-20 01:24:36 -0400

    Update mp-settings.php

--- mp-settings.php
+++ mp-settings.php
@@ -28,7 +28,7 @@ class WeDevs_Settings_API_Match {
     }
 
     function admin_menu() {
-        add_options_page( 'BP Match', 'BP Match', 'delete_posts', 'mp_bp_match', array($this, 'plugin_page') );
+        add_options_page( 'BP Match', 'BP Match', 'manage_options', 'mp_bp_match', array($this, 'plugin_page') );
     }
 
     function get_settings_sections() {
